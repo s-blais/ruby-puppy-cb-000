0fb682c48c511c562acdf45ca2ae33191b827ef4
Stephen Blais <stephen@s-blais.com>
2019-08-28 20:50:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,7 +6,7 @@ class Dog
 
   def initialize (name)
     @name = name
-    @@all << self
+    save
   end
 
   def self.all
@@ -20,5 +20,10 @@ class Dog
   def self.clear_all
     @@all.clear
   end
+
+  def save
+    @@all << self
+  end
+
 
 end
