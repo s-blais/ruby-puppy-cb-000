7eca935ec9635c8891b1c901d7342557c6352302
Stephen Blais <stephen@s-blais.com>
2019-08-28 20:29:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
-# Add your code here
+class Dog
+
+  @@all = []
+
+  attr_accessor :name
+
+  def initialize (name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    @@all {|dog| puts dog.name}
+  end
+
+end
